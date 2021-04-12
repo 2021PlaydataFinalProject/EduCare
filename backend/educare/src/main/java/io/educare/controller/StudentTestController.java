7eeacebc20a54d3132381536c3365d08ba4096b6
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-04-12 16:57:26 +0900

    Update StudentTestController.java

--- backend/educare/src/main/java/io/educare/controller/StudentTestController.java
+++ backend/educare/src/main/java/io/educare/controller/StudentTestController.java
@@ -1,5 +1,7 @@
 package io.educare.controller;
+
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -12,24 +14,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+
 import io.educare.dto.StudentTestDto;
 import io.educare.service.StudentTestService;
+
 @RestController
 @RequestMapping("/stutest")
 public class StudentTestController {
 	private final StudentTestService stTestservice;
+
 	public StudentTestController(StudentTestService stTestservice) {
 		this.stTestservice = stTestservice;
 	}
+
 	@PostMapping("/insert/{username}/{testnum}")
 	@PreAuthorize("hasAnyRole('INSTRUCTOR')")
 	public ResponseEntity<String> insertStudentTestProblem(@PathVariable String username, @PathVariable long testnum) {	//강사가 테스트에 학생 추가하는 메서드
+
 		if (stTestservice.insertStudentTest(username,testnum)) {
 			return new ResponseEntity<String>("학생 시험 매칭 등록 성공", HttpStatus.CREATED);
 		} else {
 			return  new ResponseEntity<String>("학생 시험 매칭 등록 실패", HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 	}
+
 	@GetMapping("/get/{username}/{testnum}")
 	@PreAuthorize("hasAnyRole('INSTRUCTOR','STUDENT')")
 	public ResponseEntity<StudentTestDto> getStudentTest(@PathVariable String username, @PathVariable long testnum) {
@@ -41,16 +49,19 @@ public class StudentTestController {
 	public ResponseEntity<List<StudentTestDto>> getStudentTestList(@PathVariable String username) {
 		return new ResponseEntity<List<StudentTestDto>>(stTestservice.getStudentTestList(username), HttpStatus.OK) ;
 	}
+
 	@PutMapping("/update-mytest")
 	@PreAuthorize("hasAnyRole('STUDENT')")	//답안 등록은 학생들만 가능
-	public ResponseEntity<String> updateStudentTest(StudentTestDto sttDto,
+	public ResponseEntity<String> updateStudentTest(StudentTestDto sttDto, 
 			@RequestParam(value = "file", required = false) MultipartFile mfile) {	//학생이 시험답안, 영상 제출할때 request 처리메소드
+
 		if (mfile != null && stTestservice.updateMyTest(sttDto, mfile)) {
 			return new ResponseEntity<String>("답안 작성, 녹화파일 저장 성공", HttpStatus.OK);
 		}else {
 			return new ResponseEntity<String>("답안 작성, 녹화파일 저장 실패", HttpStatus.NOT_MODIFIED);
 		}
 	}
+
 	@PutMapping("/update-score")
 	@PreAuthorize("hasAnyRole('INSTRUCTOR')")
 	public ResponseEntity<String> updateScore(@RequestBody StudentTestDto sttDto) {
@@ -61,4 +72,5 @@ public class StudentTestController {
 			return new ResponseEntity<String>("채점 결과 저장 실패",  HttpStatus.NOT_MODIFIED);		
 		}
 	}
+	
 }

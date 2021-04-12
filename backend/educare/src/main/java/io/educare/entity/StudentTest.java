b375f3951d29f465ba7d1930cb21b8116f1fac8d
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-04-13 00:16:34 +0900

    Update StudentTest
    1. LogBack 설정해서 날짜별 로그파일 생성 및 로그코드 보강
    2. ModelMapper 삭제함 (성능저하 가능성)
    3. Builder 사용으로 코드 간소화
    4. getStudentTestList 시험 응시 전에 조회시 에러 발생 해결

--- backend/educare/src/main/java/io/educare/entity/StudentTest.java
+++ backend/educare/src/main/java/io/educare/entity/StudentTest.java
@@ -1,4 +1,5 @@
 package io.educare.entity;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -9,12 +10,15 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Builder
 @Getter
 @Setter
+@AllArgsConstructor
 @NoArgsConstructor
 @Entity
 public class StudentTest{
--- backend/educare/src/main/java/io/educare/service/TestServiceimpl.java
+++ backend/educare/src/main/java/io/educare/service/TestServiceimpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.modelmapper.ModelMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -20,64 +19,65 @@ import io.educare.repository.UserRepository;
 @Service
 public class TestServiceimpl implements TestService {
 
-	private ModelMapper mapper;
-	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
-
 	private final TestRepository testRepository;
 	private final UserRepository userRepository;
 
-	public TestServiceimpl(TestRepository testRepository, UserRepository userRepository, ModelMapper mapper) {
+	public TestServiceimpl(TestRepository testRepository, UserRepository userRepository) {
 		this.testRepository = testRepository;
 		this.userRepository = userRepository;
-		this.mapper = mapper;
 	}
 
 	@Transactional
 	public Long insertTest(String username, TestDto testDto) {
 		Optional<User> userOpt = userRepository.findById(username);
-		
+
 		try {
-		if (userOpt.isPresent()) {
-			Instructor ins = (Instructor) userOpt.get();
-			Test newTest = new Test();
-			
-			newTest.setTestName(testDto.getTestName());
-			newTest.setStartTime(testDto.getStartTime());
-			newTest.setEndTime(testDto.getEndTime());
-			newTest.setTestGuide(testDto.getTestGuide());
-			newTest.setInsId(ins);
-			
-			Test savedtest = testRepository.save(newTest);
-			ins.getTestList().add(savedtest);
-			logger.info("{} 강사 시험 등록 성공", username);
-			return savedtest.getTestNum();
-		} else {
-			logger.info("{} 강사 시험 등록 실패", username);
-			return null;
-		}
-		} catch(Exception e) {
+			if (userOpt.isPresent()) {
+				Instructor ins = (Instructor) userOpt.get();
+				Test newTest = Test.builder().testName(testDto.getTestName()).startTime(testDto.getStartTime())
+						.endTime(testDto.getEndTime()).testGuide(testDto.getTestGuide()).insId(ins).build();
+
+				Test savedtest = testRepository.save(newTest);
+				ins.getTestList().add(savedtest);
+				
+				logger.info("{} 강사 시험 등록 성공", username);
+				return savedtest.getTestNum();
+			} else {
+				logger.info("{} 강사 시험 등록 실패", username);
+				return null;
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-			logger.info("{} 강사 시험 등록 실패", username);
+			logger.error("{} 강사 시험 등록 실패", username);
 			return null;
 		}
 	}
 
 	public List<TestDto> getTestsByUsername(String username) {
-
 		List<Test> testlist = testRepository.findAllTestByUsername(username);
-		List<TestDto> collect = testlist.stream().map(t -> new TestDto(t.getTestNum(), t.getTestName(), 
-				t.getStartTime(), t.getEndTime(), t.getTestGuide())).collect(Collectors.toList());
+		List<TestDto> collect = testlist.stream().map(
+				t -> new TestDto(t.getTestNum(), t.getTestName(), t.getStartTime(), t.getEndTime(), t.getTestGuide()))
+				.collect(Collectors.toList());
+		
 		logger.info("{} 강사 출제 시험 전체 조회 요청", username);
 		return collect;
 	}
 
 	public TestDto getTest(Long testnum) {
-
 		Optional<Test> testOpt = testRepository.findById(testnum);
-		TestDto testDto = mapper.map(testOpt.get(), TestDto.class);
-		logger.info("{} 시험 조회 요청", testnum);
-		return testDto;
+		
+		if(testOpt.isPresent()) {
+			Test test = testOpt.get();
+			TestDto testDto = TestDto.builder().testNum(test.getTestNum()).testName(test.getTestName())
+					.startTime(test.getStartTime()).endTime(test.getEndTime()).testGuide(test.getTestGuide()).build();
+
+			logger.info("{} 시험 조회 성공", testnum);
+			return testDto;			
+		} else {
+			logger.info("{} 시험 조회 실패", testnum);
+			return null;
+		}
 	}
 
 	@Transactional
@@ -87,7 +87,6 @@ public class TestServiceimpl implements TestService {
 		try {
 			if (testOpt.isPresent()) {
 				Test savedTest = testOpt.get();
-
 				savedTest.setStartTime(testDto.getStartTime());
 				savedTest.setEndTime(testDto.getEndTime());
 				savedTest.setTestGuide(testDto.getTestGuide());
@@ -106,10 +105,9 @@ public class TestServiceimpl implements TestService {
 			return false;
 		}
 	}
-	
+
 	@Transactional
 	public Boolean deleteTest(String username, Long testnum) {
-
 		Optional<User> userOpt = userRepository.findById(username);
 		Optional<Test> findTest = testRepository.findById(testnum);
 
@@ -117,19 +115,18 @@ public class TestServiceimpl implements TestService {
 			if (findTest.isPresent() && userOpt.isPresent()) {
 				Instructor ins = (Instructor) userOpt.get();
 				List<Test> testlist = ins.getTestList();
-				
-				for(int idx = 0; idx < testlist.size(); idx++) {
+
+				for (int idx = 0; idx < testlist.size(); idx++) {
 					if (testlist.get(idx).getTestNum() == testnum) {
 						testlist.remove(idx);
 						break;
 					}
 				}
-				
 				testRepository.delete(findTest.get());
 				logger.info("{} 시험 삭제 완료", testnum);
 				return true;
 			} else {
-				logger.error("{} 시험 삭제 실패", testnum);
+				logger.info("{} 시험 삭제 실패", testnum);
 				return false;
 			}
 		} catch (Exception e) {

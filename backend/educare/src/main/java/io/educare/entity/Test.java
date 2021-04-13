ac24216e91faa904b81ea0087f73638281fb871d
shiningcastle <54968463+shiningcastle@users.noreply.github.com>
2021-04-13 17:10:28 +0900

    Update StudentTest

--- backend/educare/src/main/java/io/educare/entity/Test.java
+++ backend/educare/src/main/java/io/educare/entity/Test.java
@@ -17,12 +17,10 @@ import javax.persistence.OneToMany;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Builder
 @Getter
 @Setter
 @AllArgsConstructor

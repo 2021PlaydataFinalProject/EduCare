748666436f5daa2703f4fd8cbd4886c24baf0664
CubeGray <kimjh1716@hanmail.net>
2021-04-16 17:26:56 +0900

    upload

--- backend/flask/server.py
+++ backend/flask/server.py
@@ -3,55 +3,92 @@ from camera import VideoCamera,RecordingThread
 from flask_mysqldb import MySQL
 # 설치! : pip install Flask-MySQLdb
 import time
+import pymysql 
 
 app = Flask(__name__)
-# database에 접근
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '0000'
-app.config['MYSQL_DB']='educare'
 
-mysql = MySQL(app)
 video_camera = None
 global_frame = None
 status = 'true'
 time_list = None
-
-# @app.route('/')
-@app.route('/<name>/<testnum>')
-def index(name, testnum):    
+userId = None
+testNum = None
+conn = pymysql.connect(host='localhost',user='root',password='0000',db='educare',charset='utf8')
+        
+@app.route('/<inputusername>/<inputtestnum>')
+def index(inputusername, inputtestnum):    
     # 쿼리문 실행
-    cur = mysql.connection.cursor()
-    cur.execute("SELECT * FROM testproblem where test_num = %s", testnum)
+    global conn
+
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM testproblem where test_num = %s", inputtestnum)
+
+    global userId
+    global testNum
+    userId = inputusername;
+    testNum = inputtestnum;
 
     fetchdata = cur.fetchall()
     prosel_list = []
+    idx = 0
     for problem in fetchdata:
-        prosel_list.append(problem[2])
-        prosel_list.append(problem[5].split(','))
+        prosel_list.append([])
+        prob_list = problem[5].split(',')
+        prosel_list[idx].append(problem[4])
+        prosel_list[idx].append(problem[2])
+        prosel_list[idx].append(prob_list[0])
+        prosel_list[idx].append(prob_list[1])
+        prosel_list[idx].append(prob_list[2])
+        prosel_list[idx].append(prob_list[3])
+        idx+=1
 
-    return render_template('index.html', data_list = prosel_list)
+    cur.execute("SELECT start_time, end_time FROM test WHERE test_num=%s",testNum)
+    fetchdata2 = cur.fetchall()
+    startTime = fetchdata2[0][0]
+    endTime = fetchdata2[0][1]
+
+    return render_template('index.html', data_list = prosel_list, user_name = userId, test_num = testNum, start_time = startTime, end_time = endTime)
 
 @app.route('/record_status', methods=['POST'])
 def record_status():
     global video_camera 
     global time_list
+    global userId
+    global testNum
+    global conn
 
     if video_camera != None:
         json = request.get_json()
         global status 
         status = json['status']
-        print("record_status==============",status) # 녹화정지 버튼 클릭시 false return, 이외 상황은 고려X    
-        # if status == "true":
-        #     video_camera.start_record()
-        #     return jsonify(result="started")
+    
+        curs = conn.cursor()
+
         if status == "false":
             video_camera.stop_record()
-            # db에 모든 정보 저장(answerlist, time_list)
-            if time_list != None:
-                print("when video finished",time_list)
-                
-            return jsonify(result="stopped")
+            
+            try :
+                if time_list !=None and userId != None and testNum != None:
+                    tmpstr = ''
+                    
+                    for time in time_list:
+                        tmpstr += time+'/'
+                    
+                    if tmpstr == '':
+                        sql = """UPDATE studenttest SET is_cheating=%s where stu_id=%s and test_num=%s""" 
+                        curs.execute(sql,('False',userId,testNum))   
+                    else :
+                        sql = """UPDATE studenttest SET cheat_time=%s, is_cheating=%s where stu_id=%s and test_num=%s"""
+                        curs.execute(sql,(tmpstr,'True',userId,testNum))
+                    
+                    conn.commit()    
+                    conn.close()
+            except Exception:
+                print(Exception)
+                conn.rollback()    
+                conn.close()
+            return jsonify(result="finished")
+            
 
 def video_stream():
     global video_camera 
@@ -74,8 +111,7 @@ def video_stream():
         else:
             yield (b'--frame\r\n'
                             b'Content-Type: image/jpeg\r\n\r\n' + global_frame + b'\r\n\r\n')
-        #print("in sever.py",time_list)
-    
+        
 
 @app.route('/video_viewer')
 def video_viewer():
